0943554f06a811ee6ea7d3cd06ea4b37874e5429
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-12-15 14:34:42 +0300

    push6

--- FrontEnd/citytours/src/LogIn/LogIn.js
+++ FrontEnd/citytours/src/LogIn/LogIn.js
@@ -1,10 +1,33 @@
+import Button from '@restart/ui/esm/Button'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../Register/Register.css'
 
 
 
 const LogIn = () =>{
 
     return(
-        <div>log in</div>
+        <div className="Container">
+        <div className="Wrapper">
+        <h1>CREATE AN ACCOUNT</h1>
+
+        <div className="Form">
+        <input placeholder="Email"></input>
+        <input placeholder="Password"></input>
+        <div className="bu">
+        <Button>SIGN in</Button>
+        </div>
+
+        {/* <div className="val">
+            <Link>DO NOT YOU REGISTER THE PASSWORD?</Link>
+            <Link>CREATE A NEW ACCOUNT</Link>
+        </div>
+         */}
+        </div>
+        </div>
+        </div>
+    
     )
 
 }
--- FrontEnd/citytours/src/Register/Register.js
+++ FrontEnd/citytours/src/Register/Register.js
@@ -1,14 +1,20 @@
+import Button from '@restart/ui/esm/Button'
 import React from 'react'
+import '../Register/Register.css'
 
 function Register() {
     return (
         <div className="Container">
         <div className="Wrapper">
-        <h3>CREATE AN ACCOUNT</h3>
+        <h1>CREATE AN ACCOUNT</h1>
 
         <div className="Form">
+        <input placeholder="Email"></input>
+        <input placeholder="Password"></input>
+        <div className="bu">
+        <Button>CREATE</Button>
+        </div>
         
-
         </div>
         </div>
         </div>
