1d28eb1dbcd7b654f2969f83d77b9e1962b5e5f5
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-12-21 15:24:44 +0300

    pushh11

--- FrontEnd/citytours/src/Footer/Footer.js
+++ FrontEnd/citytours/src/Footer/Footer.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import './Footer.css'
+import img from "../image/IMG_2107[1792] (2).PNG"
 
 export const Footer = () => {
     return (
         <div>
         
+  
+<body>
+
+<div className="section">
+<div className="main-heading">
+<h1></h1>
+<p>.</p>
+</div>
+</div>
+    <div className="footer-basic">
+        <footer>
+            <div className="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul className="list-inline">
+                <li className="list-inline-item"><a href="/">Home</a></li>
+                <li className="list-inline-item"><a href="#">Services</a></li>
+                <li className="list-inline-item"><a href="#">About</a></li>
+                <li className="list-inline-item"><a href="#">Terms</a></li>
+                <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p className="copyright">copyright ©</p>
+        </footer>
+    </div>
+    
+</body>
+
+
             
         </div>
     )
